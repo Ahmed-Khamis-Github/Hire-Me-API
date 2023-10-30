1aba5d15b326642430980ff31e0f67f83b169d2d
Ahmed <ahmed.samir00979@gmail.com>
2023-10-31 00:30:00 +0200

    Modifying data formats in company, job and review resources

--- app/Http/Resources/Front/ReviewsResource.php
+++ app/Http/Resources/Front/ReviewsResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources\Front;
 
+use DateTime;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -14,13 +15,34 @@ class ReviewsResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        // Convert the created_at timestamp to a DateTime object
+        $createdDateTime = new DateTime($this->pivot->created_at);
+        // Get the current date
+        $currentDateTime = new DateTime();
+        // Calculate the difference between the current date and the created_at date
+        $interval = $createdDateTime->diff($currentDateTime);
+
+        // Format the date based on the difference
+        if ($interval->y >= 1) {
+            $formattedDate = $interval->y . ' year' . ($interval->y > 1 ? 's' : '') . ' ago';
+        } elseif ($interval->m >= 1) {
+            $formattedDate = $interval->m . ' month' . ($interval->m > 1 ? 's' : '') . ' ago';
+        } elseif ($interval->d >= 7) {
+            $weeks = floor($interval->d / 7);
+            $formattedDate = $weeks . ' week' . ($weeks > 1 ? 's' : '') . ' ago';
+        } elseif ($interval->d >= 1) {
+            $formattedDate = $interval->d . ' day' . ($interval->d > 1 ? 's' : '') . ' ago';
+        } else {
+            $formattedDate = 'Today';
+        }
+
         return [
-            'id'=>$this->id ,
+            'id' => $this->id,
             'title' => $this->pivot->title,
-            'rating'=>$this->pivot->rating ,
-            'comment'=>$this->pivot->comment,
+            'rating' => $this->pivot->rating,
+            'comment' => $this->pivot->comment,
             'user_id' => $this->pivot->user_id,
-            'date' => $this->pivot->created_at,
+            'date' => $formattedDate,
         ];
     }
 }
